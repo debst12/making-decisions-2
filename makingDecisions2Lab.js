70cc565f83de64d498f7fbadcb512966ee024380
Spencer DeBrine <debst12@gmail.com>
2022-03-24 12:33:22 -0600

    Problems 1-7 Solutions

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -109,12 +109,19 @@ console.log(`Problem 6: ${someNum}`)
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-if (myFavoriteNumbers.length < 6) {
+
+console.log("")
+console.log("Problem 7:")
+console.log(myFavoriteNumbers.length)
+if (myFavoriteNumbers.length < 7) {
   console.log("There are not enough elements in this array.")
-} else{
-  myFavoriteNumbers.splice(6, 1, 'someNum')
+} else {
+  myFavoriteNumbers.splice(6, 1, someNum)
+  console.log(myFavoriteNumbers)
 
 }
+
+
 
 ////////// PROBLEM 8 //////////
 
